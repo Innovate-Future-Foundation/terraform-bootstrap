177627e4f6318daf35725af85734d1c79f3dc079
Yc Wang <swyc168@gmail.com>
2024-11-09 17:05:13 +1100

    UpdateModule - Rename missleading resource name in bucket module

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "bkt" {
   bucket = var.bucket_name
 
   # Not sure if this is necessary
@@ -8,14 +8,14 @@ resource "aws_s3_bucket" "terraform_state" {
   # }
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
+resource "aws_s3_bucket_versioning" "bkt" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "bkt" {
   bucket = aws_s3_bucket.terraform_state.id
   rule {
     apply_server_side_encryption_by_default {
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
+resource "aws_s3_bucket_public_access_block" "bkt" {
   bucket = aws_s3_bucket.terraform_state.id
 
   block_public_acls       = true
